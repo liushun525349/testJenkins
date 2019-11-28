41e0043ffabc0587b57b3fa43c559cfd7b56a93e
liushun <liushun@qq.com>
2019-11-28 16:50:46 +0800

    修改提交

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Description TODO
+ * @Author xu
+ * @Date 2019/11/23 14:11
+ **/
+@RestController
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello jenkins haha  "+LocalDateTime.now().toString();
+    }
+}
